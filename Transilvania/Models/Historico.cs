bbfad258f0cc57d4d2c6d9e46d9be19f7254f99a
Bruno Matheus Maia <72323650+BrunoMMaia@users.noreply.github.com>
2021-09-18 14:03:03 -0300

    Delete Transilvania directory

--- Transilvania/Models/Historico.cs
+++
@@ -1,9 +0,0 @@
-namespace Transilvania.Models
-{
-    public class Historico
-    {
-        public int Id { get; set; }
-        public string CheckIn { get; set; }
-        public string CheckOut { get; set; }
-    }
-}
